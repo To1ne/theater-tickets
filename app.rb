9c4196c12cc6ddf861893e075b4f885587b13170
Toon Claes <toon@tonotdo.com>
2013-07-03 22:39:34 +0200

    got i18n, overview working, edit is not done yet

--- app.rb
+++ app.rb
@@ -1,65 +1,60 @@
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+require 'i18n'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/dev.db")
+I18n.load_path += Dir["#{Dir.pwd}/locale/*.yml"]
+I18n.default_locale = :nl
 
-class Article
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/2014.db")
+
+class Show
   include DataMapper::Resource
 
   property :id, Serial
-  property :title, String
-  property :content, Text
+  property :date, Date
+  property :max, Integer
+  property :taken, Integer, :default => 0
 end
 
 DataMapper.finalize.auto_upgrade!
 
+if Show.all.empty?
+  show = Show.create(:date => Date.new(2014,  3,  8), :max => 170)
+  show = Show.create(:date => Date.new(2014,  3,  9), :max => 170)
+  show = Show.create(:date => Date.new(2014,  3, 15), :max => 170)
+  show = Show.create(:date => Date.new(2014,  3, 16), :max => 170)
+  show = Show.create(:date => Date.new(2014,  3, 22), :max => 170)
+  show = Show.create(:date => Date.new(2014,  3, 23), :max => 170)
+end
+
 get '/' do
+  @shows = Show.all
   erb :index
 end
 
-get '/articles' do
-  @articles = Article.all
-  erb :'articles/index'
+get '/edit' do
+  @shows = Show.all
+  erb :edit
 end
 
-get '/articles/new' do
-  erb :'articles/new'
-end
-
-get '/articles/:id' do |id|
-  @article = Article.get!(id)
-  erb :'articles/show'
-end
-
-get '/articles/:id/edit' do |id|
-  @article = Article.get!(id)
-  erb :'articles/edit'
-end
-
-post '/articles' do
-  article = Article.new(params[:article])
+# post '/articles' do
+#   article = Article.new(params[:article])
   
-  if article.save
-    redirect '/articles'
-  else
-    redirect '/articles/new'
-  end
-end
+#   if article.save
+#     redirect '/articles'
+#   else
+#     redirect '/articles/new'
+#   end
+# end
 
-put '/articles/:id' do |id|
-  article = Article.get!(id)
-  success = article.update!(params[:article])
+# put '/articles/:id' do |id|
+#   article = Article.get!(id)
+#   success = article.update!(params[:article])
   
-  if success
-    redirect "/articles/#{id}"
-  else
-    redirect "/articles/#{id}/edit"
-  end
-end
-
-delete '/articles/:id' do |id|
-  article = Article.get!(id)
-  article.destroy!
-  redirect "/articles"
-end
+#   if success
+#     redirect "/articles/#{id}"
+#   else
+#     redirect "/articles/#{id}/edit"
+#   end
+# end
